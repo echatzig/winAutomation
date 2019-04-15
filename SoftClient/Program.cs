fbc46d228c4ec4ebfbe0b109f15be577e49e2121
Elias Chatzigeorgiou <echatzig@gmail.com>
2019-04-15 13:00:21 +0300

    server/client as command line params

--- SoftClient/Program.cs
+++ SoftClient/Program.cs
@@ -10,14 +10,23 @@ namespace SoftClient
 {
     class Program
     {
-        const string url = "http://localhost:8000";
+        static string listen = ""; // example: "http://localhost:8000";
+        static string server = ""; // example: "http://localhost:9000"
 
         static void Main(string[] args)
         {
-            Console.WriteLine($"registering this client to server: {url}");
+            if (args.Length != 4 || args[0] != "-listen" || args[2] != "-connect") { 
+                Console.WriteLine($"Usage: SoftClient -listen url -connect server");
+                Console.WriteLine($"   e.g: SoftClient -listen http://localhost:8000 -connect http://localhost:9000");
+            }
 
 
-            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>(url))
+            listen = args[1];
+            server = args[3];
+            Console.WriteLine($"this client is listening at: {listen}");
+
+
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>(listen))
             {
                 var response = DoRun();
                 Console.WriteLine($"registation status: {response.Status}");
@@ -30,14 +39,15 @@ namespace SoftClient
         }
 
         static async Task<string> DoRun()
-        { 
+        {
 
+            Console.WriteLine($"connecting to server: {server}");
+            WebApp.RestClient restclient = new WebApp.RestClient(server);
 
-                WebApp.RestClient restclient = new WebApp.RestClient("http://localhost:9000");
-
-                // object initializer syntax
-                string response = await restclient.Post< string, RequestViewModel > (
-                            "api/registerclient/Register", new RequestViewModel() { url = url } 
+            Console.WriteLine($"registering this client to server: {server}");
+            // object initializer syntax
+            string response = await restclient.Post< string, RequestViewModel > (
+                            "api/registerclient/Register", new RequestViewModel() { url = listen } 
                     );
 
                 return response;
